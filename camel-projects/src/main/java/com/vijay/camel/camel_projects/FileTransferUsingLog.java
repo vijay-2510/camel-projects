87bfaf4ba3d0206e905f0df27f98119bf046c114
vijay-2510 <kumarvijay2510@gmail.com>
2019-08-29 20:01:32 +0530

    File transfer with adding logging

---
+++ camel-projects/src/main/java/com/vijay/camel/camel_projects/FileTransferUsingLog.java
@@ -0,0 +1,34 @@
+package com.vijay.camel.camel_projects;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FileTransferUsingLog {
+
+	public static void main(String[] args) {
+		CamelContext context = new DefaultCamelContext();
+
+		try {
+			context.addRoutes(new RouteBuilder() {
+
+				@Override
+				public void configure() throws Exception {
+					from("file:doc/input?noop=true")
+					.to("log:File-Tranfer-Using-Log?level=INFO&showBody=true")
+					.to("file:doc/output");
+
+				}
+			});
+
+			context.start();
+			Thread.sleep(5000);
+			context.stop();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
