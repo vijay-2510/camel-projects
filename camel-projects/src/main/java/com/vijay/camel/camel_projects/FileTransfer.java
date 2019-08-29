ce0fba936571cee55a8ed3b6eb183e84b4e37088
vijay-2510 <kumarvijay2510@gmail.com>
2019-08-29 19:36:01 +0530

    File - transfer using camel

---
+++ camel-projects/src/main/java/com/vijay/camel/camel_projects/FileTransfer.java
@@ -0,0 +1,33 @@
+package com.vijay.camel.camel_projects;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FileTransfer {
+
+	public static void main(String[] args) {
+
+		CamelContext context = new DefaultCamelContext();
+
+		try {
+			context.addRoutes(new RouteBuilder() {
+
+				@Override
+				public void configure() throws Exception {
+					from("file:doc/input?noop=true").to("file:doc/output");
+
+				}
+			});
+
+			context.start();
+			Thread.sleep(5000);
+			context.stop();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
